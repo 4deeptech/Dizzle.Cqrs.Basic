3737902b40316251a906614fe8742a129e39337b
Darren Ford <darren.ford@4deeptech.com>
2014-12-03 20:21:16 -0600

    test class generated from dsl
    test class generated from dsl

---
+++ TestDomain/Aggregates/TennisEvent.cs
@@ -0,0 +1,43 @@
+﻿using Dizzle.Cqrs.Portable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestDomain.Cqrs.Commands;
+using TestDomain.Cqrs.Events;
+
+
+namespace TestDomain.Cqrs.Model
+{
+    public partial class TennisEvent 
+    {
+        public IEnumerable<IEvent> Handle(CreateTennisEvent c)
+        {
+            yield return new TennisEventCreated(c.Id,c.USTAEventId,c.StartDate,c.EndDate,c.Name);
+        }
+
+        public IEnumerable<IEvent> Handle(UpdateTennisEvent c)
+        {
+            yield return new TennisEventUpdated(c.Id, c.USTAEventId, c.StartDate, c.EndDate, c.Name);
+        }
+
+        public void Apply(TennisEventCreated e)
+        {
+            Id = e.Id;
+            USTAEventId = e.USTAEventId;
+            StartDate = e.StartDate;
+            EndDate = e.EndDate;
+            Name = e.Name;
+        }
+
+        public void Apply(TennisEventUpdated e)
+        {
+            Id = e.Id;
+            USTAEventId = e.USTAEventId;
+            StartDate = e.StartDate;
+            EndDate = e.EndDate;
+            Name = e.Name;
+        }
+    }
+}
